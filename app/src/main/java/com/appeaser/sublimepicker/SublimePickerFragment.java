a05b00e9eb532c65aa98f2fc5b4647832a627728
Nick Jones <nick@itsnotworking.co.uk>
2018-05-24 17:18:39 +0200

    Fixing dependencies for sample app

--- app/src/main/java/com/appeaser/sublimepicker/SublimePickerFragment.java
+++ app/src/main/java/com/appeaser/sublimepicker/SublimePickerFragment.java
@@ -19,7 +19,6 @@ package com.appeaser.sublimepicker;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
